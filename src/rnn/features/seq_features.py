e3b26066aa1fb2dd53b06227abc3b878af5072fc
Cesino <norbertcesar06@gmail.com>
2025-11-18 21:56:28 +0100

    features: edited seq_features.py
    seq_features.py -> added shape features such as skew, slope, and kurtosis

--- src/rnn/features/seq_features.py
+++ src/rnn/features/seq_features.py
@@ -9,12 +9,36 @@ def _basic_stats_(windows: np.ndarray) -> Tuple[np.ndarray, list[str]]:
     min_ = windows.min(axis=1)
     max_ = windows.max(axis=1)
     range_ = max_ - min_
-    feat = np.stack([mean, std, min_, max_, range_], axis=1)
-    names = ['mean', 'std', 'min', 'max', 'range']
+    w0 = windows - windows.mean(axis=1, keepdims=True)
+    zero_crossings = (w0[:, 1:] * w0[:,-1] < 0 ).mean(axis=1)
+    feat = np.stack([mean, std, min_, max_, range_, zero_crossings], axis=1)
+    names = ['mean', 'std', 'min', 'max', 'range', 'zero_crossing_rate']
     return feat, names
 
-def _shape_feat_(windows: np.ndarray) -> Tuple[np.ndarray, list[str]]:
-    return None
+def _shape_feat_(windows: np.ndarray, fs: float) -> Tuple[np.ndarray, list[str]]:
+    time_st = windows.shape[1]
+
+    #Least Mean-Squares
+    t = (np.arange(time_st) / max(1.0, fs))[None, :]
+    t = (t-t.mean() / (t.std() + EPS))
+    x = windows - windows.mean(axis=1, keepdims=True)
+    slope = (x*t).sum(axis=1) / ((t**2).sum(axis=1) + EPS)
+    s = x.std(axis=1) + EPS
+
+    #Moment skew and Excess kurtosis
+    skew = np.mean((x/s[:, None])**3,axis=1)
+    kurt = np.mean((x/s[:, None])**4, axis=1) - 3
+
+    feat = np.stack([slope, skew, kurt], axis=1)
+    names = ['slope', 'moment_skew', 'kurt']
+    return feat, names
 
 def compute_seq_features(window: np.ndarray, include_feat1: bool = False, include_feat2: bool = False) -> Tuple[np.darray, list[str]]:
     return None
+
+
+
+
+
+
+
