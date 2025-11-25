94a19cba543abdda4bb0142940d917fc6192f104
Norbert Cesar <norbertcesar06@gmail.com>
2025-11-25 20:07:17 +0100

    feature: created model_rnn.py, train_rnn.py
    model_rnn.py -> added and managed two basic PyTorch classes for unidirectional GRU and LSTM model with small hidden layers

---
+++ src/rnn/models/model_rnn.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+
+from typing import Literal, Optional
+
+class GRUModel(nn.Module):
+    def __init__(
+            self,
+            input_dim: int,
+            hidden_dim: int,
+            num_classes: int,
+            num_layers: int = 2,
+            bidirectional: bool = False,
+            dropout: float = 0.0,
+    ):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
+
+        self.gru = nn.GRU(
+            input_size=input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            bidirectional=bidirectional,
+            dropout=dropout if num_layers > 1 else 0.0,
+        )
+
+        output_dim = hidden_dim * (2 if bidirectional else 1)
+        self.fc = nn.Linear(output_dim, num_classes)
+
+    def forward(self, x: torch.Tensor, lengths: torch.Tensor | None = None) -> torch.Tensor:
+        out, hidden = self.gru(x)
+
+        if lengths is None:
+            last = out[:, -1, :]
+        else:
+            idx = (lengths - 1).clamp(min=0)
+            B = x.size(0)
+            last = out[torch.arange(B), idx, :]
+
+        logits = self.fc(last)
+        return logits
+
+class LSTMModel(nn.Module):
+    def __init__(
+            self,
+            input_dim: int,
+            hidden_dim: int,
+            num_classes: int,
+            num_layers: int = 2,
+            bidirectional: bool = False,
+            dropout: float = 0.0,
+    ):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
+
+        self.gru = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            bidirectional=bidirectional,
+            dropout=dropout if num_layers > 1 else 0.0,
+        )
+
+        output_dim = hidden_dim * (2 if bidirectional else 1)
+        self.fc = nn.Linear(output_dim, num_classes)
+
+    def forward(self, x: torch.Tensor, lengths: torch.Tensor | None = None) -> torch.Tensor:
+        out, hidden = self.lstm(x)
+
+        if lengths is None:
+            last = out[:, -1, :]
+        else:
+            idx = (lengths - 1).clamp(min=0)
+            B = x.size(0)
+            last = out[torch.arange(B), idx, :]
+
+        logits = self.fc(last)
+        return logits
+
+
